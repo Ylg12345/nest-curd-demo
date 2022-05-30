081cc7f8e0038ce39006cd4b9a512b2ebb915aa8
闫磊刚 <2624966035@qq.com>
2022-05-30 15:55:52 +0800

    feature/头像加水印功能

--- src/server/upload/upload.service.ts
+++ src/server/upload/upload.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { baseHosts } from 'src/libs/lib';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-// const Jimp = require('jimp');
+import * as Jimp from 'jimp';
 
 const { NODE_ENV } = process.env;
 const baseHost = baseHosts[NODE_ENV] || {
@@ -13,23 +13,23 @@ const baseHost = baseHosts[NODE_ENV] || {
 @Injectable()
 export class UploadService {
   async uploadFile(upload: any) {
-    const { filename, path } = upload;
+    const { filename, path, mimetype } = upload;
     upload.uploaded = 1;
     upload.url = path.replace(baseHost.uploadPath, baseHost.baseHost);
     upload.fileName = filename;
 
     //加水印
-    /* if (mimetype.includes('image')) {
+    if (mimetype.includes('image')) {
       const text = baseHost.domain;
       Jimp.read(path).then((image) => {
         const { width, height } = image.bitmap;
-        Jimp.loadFont(Jimp.FONT_SANS_32_BLACK).then((font) => {
+        Jimp.loadFont(Jimp.FONT_SANS_16_WHITE).then((font) => {
           return image
             .print(font, width - text.length * 20, height - 50, text)
             .write(path);
         });
       });
-    } */
+    }
     return upload;
   }
 }
